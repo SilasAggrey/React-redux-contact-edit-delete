7602331a92e76f7205b8c3f5f9131d36a5532b95
SilasAggrey <91203655+SilasAggrey@users.noreply.github.com>
2022-03-07 11:07:05 -1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserContact from "./components/UserContact";
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <div>
+      <UserContact />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
